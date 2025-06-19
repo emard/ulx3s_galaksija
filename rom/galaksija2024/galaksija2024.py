b4c7caa2f0f75a616918e38dbe884bb7c1b46a5a
emard <vordah@gmail.com>
2025-06-20 01:25:58 +0200

    fix script that initializes 64K RAM with ROM content

--- rom/galaksija2024/galaksija2024.py
+++ rom/galaksija2024/galaksija2024.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 with open("galaxya42.rom","rb") as bin:
   with open("galaksija2024.mem","w") as hex:
-    for i in range(0x0000,0x2000):
-      byte = bin.read(1)
-      hex.write("%02x\n" % byte[0])
-    for i in range(0x2000,0xA000):
-      hex.write("ff\n")
-    for i in range(0xA000,0x10000):
-      byte = bin.read(1)
-      if not byte:
-        break
-      hex.write("%02x\n" % byte[0])
+    for j in range(4):
+      for i in range(0x0000+j*0x4000,0x4000+j*0x4000):
+        byte = bin.read(1)
+        if not byte:
+          break
+        hex.write("%02x\n" % byte[0])
+      for k in range(i,0x4000+j*0x4000):
+        hex.write("%02x\n" % 0xff)
+      bin.seek(0)
     hex.close()
   bin.close()
