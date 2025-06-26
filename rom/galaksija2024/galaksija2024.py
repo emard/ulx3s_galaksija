eb748c80ebc017540df6bcf02355b8095390c5f4
emard <vordah@gmail.com>
2025-06-26 17:05:00 +0200

    galaksija 2024 cleaned-up conversion script. converts eprom binary image "galaxya42.rom" to verilog BRAM initializer read by $readmemh("galaksija2024.mem"). Because eprom is 32K then initializer content now repeats after 32K. RAM mapped area 0x2000-0x9FFF is initialized as 0xFF.

--- rom/galaksija2024/galaksija2024.py
+++ rom/galaksija2024/galaksija2024.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python3
 with open("galaxya42.rom","rb") as bin:
   with open("galaksija2024.mem","w") as hex:
-    for j in range(4):
-      for i in range(0x0000+j*0x4000,0x4000+j*0x4000):
+    addr = 0
+    for j in range(2):
+      for i in range(0x0000+j*0x8000,0x8000+j*0x8000):
         byte = bin.read(1)
         if not byte:
           break
-        hex.write("%02x\n" % byte[0])
-      for k in range(i,0x4000+j*0x4000):
+        if addr < 0x2000 or addr >= 0xA000:
+          hex.write("%02x\n" % byte[0])
+        else:
+          hex.write("%02x\n" % 0xff)
+        addr += 1
+      for k in range(i,0x8000+j*0x8000):
         hex.write("%02x\n" % 0xff)
+        addr += 1
       bin.seek(0)
     hex.close()
   bin.close()
